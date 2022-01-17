a83f5e85c40e5cfe745e00a16bbacaaf38a88720
yzjiangyang <yzjiangyang123@163.com>
2022-01-17 13:35:19 -0800

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import { Route, Switch } from 'react-router-dom'
 import './App.css'
 
 function App() {
 	return (
 		<div className="App">
+			{/* header is always present despite the page */}
+			<Header /> 
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route path="/shop" component={ShopPage} />
