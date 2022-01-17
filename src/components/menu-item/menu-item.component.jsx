bcecaf127cec6b61b720bffb4c51dc7bc07fb61d
yzjiangyang <yzjiangyang123@163.com>
2022-01-16 22:45:28 -0800

    react router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router'
 
 const MenuItem = (props) => (
-    <div className={`${props.size} menu-item`}>
+    <div
+        className={`${props.size} menu-item`}
+        onClick={() => props.history.push(`${props.match.path}${props.linkUrl}`)}
+    >
         <div className="background-image" style={{ backgroundImage:`url(${props.imageUrl})` }}>
         </div>
         <div className="content">
@@ -13,4 +17,4 @@ const MenuItem = (props) => (
 )
 
 
-export default MenuItem
+export default withRouter(MenuItem)
